06b51ce1a27fdff21e86f93f5f99e58e48714b3a
Jason Johnston <jasonjoh@microsoft.com>
2021-12-01 17:45:04 -0500

    Initial files

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+import * as core from '@actions/core';
+
+async function run(): Promise<void> {
+  core.info(`Hello world!`);
+}
+
+run();
